f9f40b26bd6a881056ef65073e9bc45a3d354f5c
Amol Chakane <Amol.Chakane@harbingergroup.com>
2022-04-08 17:57:02 +0530

    Login using email (username) and password, API to return token

--- apis/views.py
+++ apis/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework. settings import api_settings
+
 from apis import serializers
 from apis import models
 from apis import permissions
@@ -18,3 +21,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email', 'phone',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
